54f8ee3b7c5184a72046b29a949844dacde6589e
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-27 21:32:47 +0100

    Delete resources directory

--- resources/cypher.fish
+++
@@ -1,4 +0,0 @@
-CALL apoc.load.xls('http://bit.ly/2nXgHA2','Kids')
-CALL apoc.load.xls('test_date.xlsx','sheet',{mapping:{Date:{type:'String'}}})
-CALL apoc.load.xls('test_date.xlsx','sheet',{mapping:{Date:{type:'String'}}})
-CALL apoc.load.xls('test_date.xlsx','sheet',{mapping:{Date:{type:'String',dateParse:["wrongPath", "dd-MM-yyyy", "dd/MM/yyyy", "yyyy/MM/dd", "yyyy/dd/MM", "yyyy-dd-MM'T'hh:mm:ss"]}}})
